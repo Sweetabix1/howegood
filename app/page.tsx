37f645f100a697c3b885868c9869c6bc53b28470
Dan Sweeney <dsweeney@cergenx.com>
2023-12-30 19:16:33 +0000

    more responsive tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -34,47 +34,52 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-start bg-stone-950">
       {waveData && windData ? (
         <div className="flex flex-col gap-4 p-8 sm:p-12 md:p-16 lg:p-24 pb-8 sm:pb-8 md:pb-0 lg:pb-0">
-          <p className="text-stone-400 text-xs">Coordinates: 51.56, -8.64</p>
-          <h1 className="text-2xl font-bold text-stone-100">Current forecast</h1>
-          <div className="flex flex-col gap-1">
-            <p className="text-stone-400 text-xs">Wave:</p>
-            <div className="flex gap-3 items-center">
-              <div className="flex gap-2 items-center">
-                <Arrow angle={waveData?.hourly?.wave_direction[hrs]} />
+          <div className="flex flex-col gap-2">
+            <p className="text-stone-400 text-xs">Coordinates: 51.56, -8.64</p>
+            <h1 className="text-2xl font-bold text-stone-100">Current forecast</h1>
+          </div>
+          <div className="flex flex-col gap-4 md:flex-row md:gap-8">
+            <div className="flex flex-col gap-1">
+              <p className="text-stone-400 text-xs">Wave:</p>
+              <div className="flex gap-3 items-center">
+                <div className="flex gap-2 items-center">
+                  <Arrow angle={waveData?.hourly?.wave_direction[hrs]} />
+                  <p className="text-stone-100 font-bold">
+                    {waveData?.hourly?.wave_direction[hrs]}
+                    <span className="text-stone-400 font-regular">°</span>
+                  </p>
+                </div>
                 <p className="text-stone-100 font-bold">
-                  {waveData?.hourly?.wave_direction[hrs]}
-                  <span className="text-stone-400 font-regular">°</span>
+                  {(waveData?.hourly?.wave_height[hrs] * 3.2808).toFixed(1)}
+                  <span className="text-stone-400 font-regular text-sm">ft</span>
+                </p>
+                <p className="text-stone-400">@</p>
+                <p className="text-stone-100 font-bold">
+                  {manipulatePeriod(waveData?.hourly?.wave_period[hrs])}
+                  <span className="text-stone-400 font-regular text-sm">s</span>
                 </p>
               </div>
-              <p className="text-stone-100 font-bold">
-                {(waveData?.hourly?.wave_height[hrs] * 3.2808).toFixed(1)}
-                <span className="text-stone-400 font-regular text-sm">ft</span>
-              </p>
-              <p className="text-stone-400">@</p>
-              <p className="text-stone-100 font-bold">
-                {manipulatePeriod(waveData?.hourly?.wave_period[hrs])}
-                <span className="text-stone-400 font-regular text-sm">s</span>
-              </p>
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <p className="text-stone-400 text-xs">Wind:</p>
+              <div className="flex gap-3 items-center">
+                <div className="flex gap-2 items-center">
+                  <Arrow angle={windData?.hourly?.wind_direction_10m[hrs]} />
+                  <p className="text-stone-100">
+                    {windData?.hourly?.wind_direction_10m[hrs]}
+                    <span className="text-stone-400 font-regular">°</span>
+                  </p>
+                </div>
+                <p className="text-stone-100 font-bold">
+                  {windData?.hourly?.wind_speed_10m[hrs]}
+                  <span className="text-stone-400 font-regular text-sm">kmph</span>
+                </p>
+                <p className="text-stone-100 font-bold"></p>
+              </div>
             </div>
           </div>
 
-          <div className="flex flex-col gap-1">
-            <p className="text-stone-400 text-xs">Wind:</p>
-            <div className="flex gap-3 items-center">
-              <div className="flex gap-2 items-center">
-                <Arrow angle={windData?.hourly?.wind_direction_10m[hrs]} />
-                <p className="text-stone-100">
-                  {windData?.hourly?.wind_direction_10m[hrs]}
-                  <span className="text-stone-400 font-regular">°</span>
-                </p>
-              </div>
-              <p className="text-stone-100 font-bold">
-                {windData?.hourly?.wind_speed_10m[hrs]}
-                <span className="text-stone-400 font-regular text-sm">kmph</span>
-              </p>
-              <p className="text-stone-100 font-bold"></p>
-            </div>
-          </div>
           <div className="flex flex-col gap-1">
             <p className="text-stone-400 text-xs">Score:</p>
             <p className="text-stone-100 font-bold text-5xl">
@@ -88,7 +93,7 @@ export default function Home() {
       )}
       {waveData && windData && (
         <div className="flex flex-col gap-4 w-full p-0 md:p-16 lg:p-24">
-          <h2 className="text-xl font-bold text-stone-100 pl-8 sm:pl-12 md:pl-0">Hourly forecast:</h2>
+          <h2 className="text-lg font-bold text-stone-100 pl-8 sm:pl-12 md:pl-0">Hourly forecast:</h2>
           <ForecastTable waveData={waveData} windData={windData} getScore={getScore} />
         </div>
       )}
